7bf9350c9e7340d6939d80d147a707c044277aa2
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-05-07 14:40:54 -0500

    Encuesta ViewSet

--- src/encuesta/views.py
+++ src/encuesta/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Encuesta, Pregunta, Opcion, Respuesta
+from .serializers import EncuestaSerializer, PreguntaSerializer, OpcionSerializer, RespuestaSerializer
 
-# Create your views here.
+class EncuestaViewSet(viewsets.ModelViewSet):
+    queryset = Encuesta.objects.all()
+    serializer_class = EncuestaSerializer
+    permission_classes = [permissions.AllowAny]
