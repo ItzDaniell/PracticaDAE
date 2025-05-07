9c1082eef98d90d2b25c89683154b95281b64651
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-05-07 14:41:16 -0500

    Pregunta ViewSet

--- src/encuesta/views.py
+++ src/encuesta/views.py
@@ -7,3 +7,8 @@ class EncuestaViewSet(viewsets.ModelViewSet):
     queryset = Encuesta.objects.all()
     serializer_class = EncuestaSerializer
     permission_classes = [permissions.AllowAny]
+
+class PreguntaViewSet(viewsets.ModelViewSet):
+    queryset = Pregunta.objects.all()
+    serializer_class = PreguntaSerializer
+    permission_classes = [permissions.AllowAny]
